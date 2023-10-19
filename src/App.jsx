fad59cba77ace90d67943b541722475694b738ac
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-10-19 15:37:43 -0300

    Adição de arquivos e dependncias

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { Flex } from "@chakra-ui/react"
+import { Outlet } from "react-router-dom"
+
 const App = () => {
   return (
-    <div>App</div>
+    <Flex>
+      <Outlet />
+    </Flex>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import theme from './utils/theme.js'
 import './index.css'
+import Home from './routes/Home.jsx'
+import Panel from './routes/Panel.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/panel', element: <Panel /> }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
